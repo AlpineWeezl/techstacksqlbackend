4e8532dc0288f05f77236205eb06a3bced9d1ed9
Matthias Wieser <matthias.wieser.83@live.de>
2022-05-19 17:53:01 +0200

    adjust cors settings

--- routes/techs.js
+++ routes/techs.js
@@ -2,18 +2,26 @@ import Router from "express";
 import { createTech, deleteTechById, getAllTechs, getTechById, getTechsByCategoryId, updateTechById } from "../controllers/techs.js";
 
 export const techsRouter = Router();
+const error = () => {
+   return({
+        message: 'Opps, something went wrong!'
+    });
+};
 
 techsRouter
     .route('/')
     .get(getAllTechs)
-    .post(createTech);
+    .post(createTech)
+    .all(error);
 
 techsRouter
     .route('/:id')
     .get(getTechById)
     .put(updateTechById)
-    .delete(deleteTechById);
+    .delete(deleteTechById)
+    .all(error);
 
 techsRouter
     .route('/category/:cat_id')
     .get(getTechsByCategoryId)
+    .all(error);
